4d0ce1c9060fbce2ab357c98f0054faa207a6260
Pietro Moro <pietromoro@programmer.net>
2020-03-23 21:45:25 +0100

    Add service helper methods to setup the lookup of files

--- src/ev3dev/device.rb
+++ src/ev3dev/device.rb
@@ -6,13 +6,48 @@ module Ev3dev
       @device_path = path
     end
 
+    def self.lookup_files *files
+      files.each do |f|
+        file = File.join device_path, f
+        raise ArgumentError unless File.exist? file
+        define_method f do
+          read(file)
+        end
+        define_method "#{f}=" do |value|
+          write(file, value: value)
+        end
+      end
+    end
+
+    def self.read_only *files
+      files.each do |f|
+        file = File.join device_path, f
+        raise ArgumentError unless File.exist? file
+        define_method f do
+          read(file)
+        end
+      end
+    end
+
+    def self.write_only *files
+      files.each do |f|
+        file = File.join device_path, f
+        raise ArgumentError unless File.exist? file
+        define_method "#{f}=" do |value|
+          write(f, value: value)
+        end
+      end
+    end
+
     def write(file, value:)
       file = File.join @device_path, file.to_s
+      raise "No such file #{file}" unless File.exist? file
       IO.write file, value
     end
 
     def read(file)
       file = File.join @device_path, file.to_s
+      raise "No such file #{file}" unless File.exist? file
       IO.read(file).strip
     end
   end
