07f417ff788855328f396e2e9992ccc43f37b1a6
Pietro Moro <pietromoro@programmer.net>
2020-03-24 22:32:11 +0100

    Abstract led class

---
+++ src/ev3dev/led.rb
@@ -0,0 +1,14 @@
+module Ev3dev
+  class Led < Device
+    PATH = "/sys/class/leds/"
+
+    lookup_files :max_brightness, :trigger, read: true
+    lookup_files :brightness, read: true, write: true
+
+    def initialize(side, color)
+      raise ArgumentError unless [0, 1].include? side
+      raise ArgumentError unless %w(red green).include? color
+      super PATH + "led#{size}\:#{color}\:brick-status/"
+    end
+  end
+end
