b2d0d9e7b7ebd45b6367e6215070b24b3bd179dc
Pietro Moro <pietromoro@programmer.net>
2020-03-25 10:43:29 +0100

    Tacho motor class

---
+++ src/ev3dev/tacho_motor.rb
@@ -0,0 +1,12 @@
+module Ev3dev
+  class TachoMotor
+    PATH = "/sys/class/tacho-motor/motor"
+
+    lookup_files :address, :commands, :count_per_rot, :driver_name, :duty_cycle, :max_speed, :position, :speed, :state, :stop_actions, read: true
+    lookup_files :command, :duty_cycle_sp, :polarity, :position_sp, :ramp_down_sp, :ramp_up_sp, :speed_sp, :stop_action, :time_sp, read: true, write: true
+
+    def initialize(number)
+      super PATH + number
+    end
+  end
+end
