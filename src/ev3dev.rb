b05883476a86f88eb36bf79653a0f040f7cdd472
Pietro Moro <pietromoro@programmer.net>
2020-03-25 11:48:13 +0100

    Update ev3dev module definition

--- src/ev3dev.rb
+++ src/ev3dev.rb
@@ -1,9 +1,9 @@
 module Ev3dev
 end
 
-require_relative "device"
-require_relative "battery"
-require_relative "led"
-require_relative "port"
-require_relative "sensor"
-require_relative "tacho_motor"
+require_relative "ev3dev/device"
+require_relative "ev3dev/battery"
+require_relative "ev3dev/led"
+require_relative "ev3dev/port"
+require_relative "ev3dev/sensor"
+require_relative "ev3dev/tacho_motor"
