7705d0ab0778530e697348bc73241152111e70a8
Pietro Moro <pietromoro@programmer.net>
2020-04-18 22:37:53 +0200

    Simple server setup

--- ev3dev-server.rb
+++ ev3dev-server.rb
@@ -1,3 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require_relative 'src/websocket/server'
+
+puts "Initializin..."
+server = WebSocket::Server.new
+
+puts "Ready to accept requests"
+
+loop do
+  Thread.new(server.accept) do |connection|
+    puts "Connected"
+    while (message = connection.receive)
+      puts "Received #{message}"
+      connection.send("Received #{message}...")
+    end
+  end
+end
