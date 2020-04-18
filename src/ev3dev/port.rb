31418c26f82be3fb70a466380c8396c09d246482
Pietro Moro <pietromoro@programmer.net>
2020-04-18 10:46:07 +0200

    Define autoscanning feature

--- src/ev3dev/port.rb
+++ src/ev3dev/port.rb
@@ -1,13 +1,15 @@
 module Ev3dev
   class Port < Device
-    PATH = "/sys/class/lego-port/port"
+    PATH = "/sys/class/lego-port/"
 
     lookup_files :address, :driver_name, :modes, read_once: true
     lookup_files :status, read: true
     lookup_files :mode, :set_device, read: true, write: true
 
+    setup_autoscan PATH
+
     def initialize(port)
-      super PATH + port.to_s
+      super PATH + port
     end
   end
 end
