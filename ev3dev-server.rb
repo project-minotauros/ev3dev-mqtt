fe3398d035c21285d4dc28d57e97b686bf462def
Pietro Moro <pietromoro@programmer.net>
2020-04-24 21:54:38 +0200

    Message handler hookup

--- ev3dev-server.rb
+++ ev3dev-server.rb
@@ -1,19 +1,26 @@
+#!/usr/local/bin/ruby -w
+
+puts "Starting up..."
+
 require 'rubygems'
 require 'bundler/setup'
 
 require_relative 'src/websocket/server'
+require_relative 'stc/message_handler'
 
-puts "Initializin..."
+puts "Initializing..."
 server = WebSocket::Server.new host: '0.0.0.0'
 
 puts "Ready to accept requests"
 
 loop do
   Thread.new(server.accept) do |connection|
-    puts "Connected"
+    puts "A client has connected"
+    handler = MessageHandler.new connection
     while (message = connection.receive)
       puts "Received #{message}"
       connection.send("Received #{message}...")
+      handler.decode message
     end
   end
 end
