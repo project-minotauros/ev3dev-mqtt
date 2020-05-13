fac2ac08db676f27c0106050a23a8a9ef1654cf9
Pietro Moro <pietromoro@programmer.net>
2020-05-13 14:40:11 +0200

    Refactor Device base class

--- src/ev3dev/device.rb
+++ src/ev3dev/device.rb
@@ -3,36 +3,7 @@ module Ev3dev
     attr_reader :device_path
 
     def initialize(path)
-      @read_once_files = []
       @device_path = path
-    end
-
-    def read_once_attributes
-      @read_once_files.map do |file|
-        { file.to_sym => send(file.to_sym) }
-      end.inject ({}) do |result, object|
-        result.merge(object)
-      end
-    end
-
-    def self.lookup_files *files, read: false, write: false, read_once: false
-      files.each do |file|
-        if read
-          define_method file do
-            read(file)
-          end
-        elsif read_once
-          @read_once_files.append(file.to_sym)
-          define_method file do
-            instance_variable_get("@#{file}") || instance_variable_set("@#{file}", read(file))
-          end
-        end
-        if write
-          define_method "#{file}=" do |value|
-            write(file, value: value)
-          end
-        end
-      end
     end
 
     class << self
@@ -43,8 +14,42 @@ module Ev3dev
           end
         end
       end
+
+      def lookup_files *files, read: false, write: false, read_once: false
+        read_once_files = []
+        files.each do |file|
+          if read
+            define_method file do
+              read(file)
+            end
+          elsif read_once
+            read_once_files << file
+            define_method file do
+              instance_variable_get("@#{file}") || instance_variable_set("@#{file}", read(file))
+            end
+          end
+          if write
+            define_method "#{file}=" do |value|
+              write(file, value: value)
+            end
+          end
+        end
+        define_read_onces(read_once_files)
+      end
     end
 
+  private
+    def self.define_read_onces(read_onces)
+      define_method(:read_once_attributes) do
+        read_onces.map do |file|
+          { file.to_sym => send(file.to_sym) }
+        end.inject ({}) do |result, object|
+          result.merge(object)
+        end
+      end
+    end
+
+  protected
     def write(file, value:)
       file = File.join @device_path, file.to_s
       raise "No such file #{file}" unless File.exist? file
